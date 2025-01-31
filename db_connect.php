5a458c544efe87862e61e44368c9f5edb3adef06
Christian06 <123466416+Hatdoglang@users.noreply.github.com>
2025-01-31 21:50:33 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,15 @@
-<?php 
+<?php
+$host = "b8occq7i8qfvinaczwwh-mysql.services.clever-cloud.com";
+$user = "uuovfe0ukxs2luyy";  // Updated username (lowercase "y" at the end)
+$password = "sZXlCFtPlpIwOU0IzLK8";  // Updated password (zero instead of 'O')
+$dbname = "b8occq7i8qfvinaczwwh";
+$port = 3306;
 
-$conn= new mysqli('localhost','root','','evaluation_db')or die("Could not connect to mysql".mysqli_error($con));
+// Create MySQL connection
+$conn = new mysqli($host, $user, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
